a8c356885038ce91cd6d943f44623cb7db70be64
SaRi <Pharuehat.ka.62@ubu.ac.th>
2022-07-04 18:13:19 +0700

    add savefunction

--- src/module/motor-information/motor-information.controller.ts
+++ src/module/motor-information/motor-information.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateCurrentRecord } from './interface/create-current-record.interface';
 import { MotorInformationService } from './motor-information.service';
 
-@Controller()
+@Controller('motor')
 export class MotorInformationController {
   constructor(private readonly motorInfoService: MotorInformationService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.motorInfoService.getHello();
+  }
+  @Post('save')
+  saveData(@Body() body:CreateCurrentRecord ){
+    this.motorInfoService.SaveMotorData(body)
+    return{
+      success:'success'
+    }
   }
 }
--- src/module/motor-information/motor-information.service.ts
+++ src/module/motor-information/motor-information.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { MotorInformationDal } from './motor-information.dal';
-
+import { CreateCurrentRecord } from './interface/create-current-record.interface'
 @Injectable()
 export class MotorInformationService {
   constructor(private motorInformationDal: MotorInformationDal) {}
   getHello(): string {
     return 'Hello World!';
+  }
+  SaveMotorData(metaData: CreateCurrentRecord){
+    this.motorInformationDal.addLog(metaData)
   }
 }
