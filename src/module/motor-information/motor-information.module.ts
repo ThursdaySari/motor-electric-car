1364a6219bcb3505dbae22b0c7a04e0077122928
Songphon Chamniansong <songphon.cha@ondemand.in.th>
2022-07-04 17:28:37 +0700

    Fixed build Error

--- src/module/motor-information/motor-information.module.ts
+++ src/module/motor-information/motor-information.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { MotorInformationController } from './motor-information.controller';
+import { MotorInformationDal } from './motor-information.dal';
 import { MotorInformationService } from './motor-information.service';
+import {
+  MotorInformation,
+  MotorInformationSchema,
+} from '../../schema/motor-information.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([
+      { name: MotorInformation.name, schema: MotorInformationSchema },
+    ]),
+  ],
   controllers: [MotorInformationController],
-  providers: [MotorInformationService],
+  providers: [MotorInformationService, MotorInformationDal],
 })
 export class MotorInformationModule {}
